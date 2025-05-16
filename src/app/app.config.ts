7c6d2e5c3f644c41cc196a82f9eee83955bc4030
mohannad74 <mohanadashraf001@gmail.com>
2025-05-16 03:02:26 +0300

    .

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,4 +4,5 @@ import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  // comment
 };
